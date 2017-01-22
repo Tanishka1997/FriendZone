8a39c8ead2b9345b3580b9b7cfc2caf48feeda44
Tanishka1997 <u15co059@coed.svnit.ac.in>
2017-01-22 15:47:10 +0530

    twitter login eneabled

--- config.py
+++ config.py
@@ -8,5 +8,9 @@ OAUTH_CREDENTIALS= {
     'facebook': {
         'id': '1273624476010013',
         'secret': 'c0da4260c2755a3c83a87edfe4e4a8e5'
+    },
+    'twitter':{
+        'id': 'lx87hh7H6h2QriIsWwHVb77Rd',
+        'secret':'Ck9bmew2FFq7X2ay7GH2tRsNZcEuKbHMko6OBFwLffbR0Ha8bB'
     }
-    }
+}
